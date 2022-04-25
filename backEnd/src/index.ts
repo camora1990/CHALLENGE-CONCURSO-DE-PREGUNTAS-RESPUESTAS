b9267d10eff61c18937f18674a87287b76454af6
camora1990 <moorales.camilo@gmail.com>
2022-04-25 15:21:00 -0500

    [add] implementacion de funcion para inicializar el servidor y la conexion a base de datos

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -1,5 +1,16 @@
+import { Server } from "./bootstrap/Server.bootstrap";
+import { DataBase } from "./bootstrap/DataBase.bootstrap";
 
+const server = new Server();
+const dataBase = new DataBase();
 
-const a : number = 125
-
-console.log(a)
+(async function initServer() {
+  try {
+    await server.initialize();
+    const mongoose = await dataBase.initialize();
+    console.log(`connected to dataBase ${mongoose.connections[0].name}`);
+  } catch (error) {
+    await dataBase.closeConnection()
+    console.log(error);
+  }
+})();
