4fdf42b5860fc473946720a49ee3a6533d2590ec
camora1990 <moorales.camilo@gmail.com>
2022-04-25 17:28:05 -0500

    [add] datos iniciales  colleccion Round

---
+++ backEnd/src/data/round.initalData.ts
@@ -0,0 +1,19 @@
+import { Round } from "../model/Round.model";
+
+export const roundInitialData: Array<Round> = [
+  {
+    round: "ROUND #1",
+  },
+  {
+    round: "ROUND #2",
+  },
+  {
+    round: "ROUND #3",
+  },
+  {
+    round: "ROUND #4",
+  },
+  {
+    round: "ROUND #4",
+  },
+];
