cfa56789879d5b1e441b4bb30d9ed6ee1c40530f
camora1990 <moorales.camilo@gmail.com>
2022-04-25 17:42:54 -0500

    [add] funcion creacion data inicial y eliminacion de data

--- backEnd/src/bootstrap/DataBase.bootstrap.ts
+++ backEnd/src/bootstrap/DataBase.bootstrap.ts
@@ -1,6 +1,8 @@
 import { IServer } from "./IServer.bootstrap";
 import mongosee, { Connection } from "mongoose";
 import config from "../config/config";
+import {  roundModel } from "../model/Round.model";
+import { roundInitialData } from "../data/round.initalData";
 
 export class DataBase implements IServer {
   private _CONNECTION_STRING: string;
@@ -28,6 +30,23 @@ export class DataBase implements IServer {
   async closeConnection(): Promise<void> {
     try {
       await this._dbClient.close(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async initializeData(): Promise<void> {
+    try {
+      await this.deleteData();
+      const createRound = roundModel.insertMany(roundInitialData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteData(): Promise<void> {
+    try {
+      await roundModel.deleteMany();
     } catch (error) {
       console.log(error);
     }
