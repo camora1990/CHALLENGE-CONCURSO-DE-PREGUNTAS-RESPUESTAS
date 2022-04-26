ee1c2ad07bcad701fb327b61e7bf9fe85cb8656a
camora1990 <moorales.camilo@gmail.com>
2022-04-26 12:00:40 -0500

    [Add] se implementa logica de insercion de datos iniciales

--- backEnd/src/helpers/populateData.ts
+++ backEnd/src/helpers/populateData.ts
@@ -1,6 +1,8 @@
 import { categoryInitialData } from "../data/category.initialData";
+import { questionInitialData } from "../data/question.initialData";
 import { roundInitialData } from "../data/round.initalData";
 import categoryModel from "../model/Category.model";
+import questionModel from "../model/Question.model";
 import { roundModel } from "../model/Round.model";
 
 export const populateData = async (): Promise<void> => {
@@ -8,7 +10,7 @@ export const populateData = async (): Promise<void> => {
   let categories: any[] = [];
   createRound.forEach(({ round, _id }) => {
     categoryInitialData
-      .filter((category) => category.round === round)
+      .filter((category) => category.round.round === round)
       .forEach(({ category }) => {
         categories.push({
           category,
@@ -16,5 +18,17 @@ export const populateData = async (): Promise<void> => {
         });
       });
   });
-  await categoryModel.insertMany(categories);
+  const newCategories = await categoryModel.insertMany(categories);
+  let questions: any[] = [];
+  newCategories.forEach(({ category, _id }) => {
+    questionInitialData
+      .filter((question) => question.category.category === category)
+      .forEach((question) => {
+        questions.push({
+          ...question,
+          category: _id,
+        });
+      });
+  });
+  await questionModel.insertMany(questions);
 };
