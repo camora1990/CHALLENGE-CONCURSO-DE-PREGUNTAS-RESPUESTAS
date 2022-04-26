3fbf9dfca14189cc0ff8e464d6d6d1f3ecd5e583
camora1990 <moorales.camilo@gmail.com>
2022-04-25 23:47:02 -0500

    [Add] data inicial para categorias y rondas

---
+++ backEnd/src/data/category.initialData.ts
@@ -0,0 +1,23 @@
+export const categoryInitialData: Array<any> = [
+  {
+    category: "Fundamentos de programación",
+    round: "ROUND #1",
+  },
+  {
+    category: "Git",
+    round: "ROUND #2",
+  },
+  {
+    category: "Desarrollo Front End",
+    round: "ROUND #4",
+  },
+  {
+    category: "Programacion orientada a objetos",
+    round: "ROUND #3",
+  },
+
+  {
+    category: "Bases de datos",
+    round: "ROUND #5",
+  },
+];
--- backEnd/src/data/round.initalData.ts
+++ backEnd/src/data/round.initalData.ts
@@ -2,23 +2,23 @@ import { Round } from "../model/Round.model";
 
 export const roundInitialData: Array<Round> = [
   {
-    round: "ROUND #1",
+    round: "RONDA #1",
     points: 10000,
   },
   {
-    round: "ROUND #2",
+    round: "RONDA #2",
     points: 15000,
   },
   {
-    round: "ROUND #3",
+    round: "RONDA #3",
     points: 20000,
   },
   {
-    round: "ROUND #4",
+    round: "RONDA #4",
     points: 25000,
   },
   {
-    round: "ROUND #5",
+    round: "RONDA #5",
     points: 30000,
   },
 ];
