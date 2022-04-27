a6589d021922b4d7854c68f4b56724cf2d2f8872
camora1990 <moorales.camilo@gmail.com>
2022-04-26 22:37:25 -0500

    [add] se implementa el memo y el logaout de la aplicacion

--- frontend/src/components/ui/Nav.jsx
+++ frontend/src/components/ui/Nav.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
-export const Nav = () => {
+import PropTypes from "prop-types";
+import { types } from "../../types/types";
+
+export const Nav = React.memo(({ puntos = 0 }) => {
+  const { player, dispatch } = useContext(AuthContext);
+  const handleClick = () => {
+    dispatch({
+      type: types.logout,
+    });
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container d-flex flex-wrap justify-content-between">
         <div className="d-flex flex-wrap text-light">
           <span className="nav-item nav-link text-secondary">
-            CAMILO MORALES
+            {player.name}
           </span>
         </div>
         <div className="d-flex flex-wrap">
-          <span className="nav-item nav-link text-success">Total Puntos: text-success</span>
-          <button className="btn btn-outline-secondary">Salir</button>
+          <span className="nav-item nav-link text-success">
+            Total Puntos: {puntos}
+          </span>
+          <button className="btn btn-outline-secondary" onClick={handleClick}>
+            Salir
+          </button>
         </div>
       </div>
     </nav>
   );
+});
+
+Nav.propTypes = { puntos: PropTypes.number.isRequired };
+Nav.defaultProps = {
+  puntos: 0,
 };
