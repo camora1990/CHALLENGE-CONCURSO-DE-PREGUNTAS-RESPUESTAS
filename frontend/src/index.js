b4a5297729bcf6ea91c135363b33c54a2ba84387
camora1990 <moorales.camilo@gmail.com>
2022-04-26 21:17:20 -0500

    [Add] configuracion de ruta por defecto para axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import axios from 'axios'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+axios.defaults.baseURL = "http://localhost:8081/api/v1"
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
