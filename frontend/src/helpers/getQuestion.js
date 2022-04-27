20ed8b6efede353bd5c979f5f0dcca4ab9295c2a
camora1990 <moorales.camilo@gmail.com>
2022-04-27 12:15:56 -0500

    [add] se implementa random para opciones aleatorias

--- frontend/src/helpers/getQuestion.js
+++ frontend/src/helpers/getQuestion.js
@@ -15,7 +15,7 @@ export const getQuestion = async (rondaId) => {
       Math.random() * (responseQuestons.question.length - 1 - 0 + 1) + 0
     );
     return {
-      opciones: responseQuestons.question[random].options,
+      opciones: responseQuestons.question[random].options.sort(()=>Math.random()-0.5),
       pregunta: responseQuestons.question[random].question,
       categoria: responseCategory.category.category,
     };
